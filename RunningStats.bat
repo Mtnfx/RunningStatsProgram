7c508b0123d62b45b8ca87be8147cec6fc284775
Mtnfx <77549405+Mtnfx@users.noreply.github.com>
2021-01-20 20:39:16 -0500

    Update RunningStats.bat

--- RunningStats.bat
+++ RunningStats.bat
@@ -1,3 +1,3 @@
-cd Documents :: Replace this
+cd Documents :: Replace this with the directory for where the python and r file are on the local system.
 running_stats_skeleton.py
 "C:\Program Files\R\R-4.0.3\bin\Rscript.exe" C:\Users\mtnfx\Documents\RunningDataProcess.r :: The first directory (in quotes) should be the address your r version's .exe and the second unquoted directory should point to the place you put RunningDataProcess.r
